6b4d9f0c95311f0c01481958942cb4bae554c5bc
sapir <sapirduke@gmail.com>
2014-11-09 23:38:07 +0200

    exercise 2

--- src/com/mta/javacourse/SapirfirstprojectServlet.java
+++ src/com/mta/javacourse/SapirfirstprojectServlet.java
@@ -5,7 +5,14 @@ import javax.servlet.http.*;
 @SuppressWarnings("serial")
 public class SapirfirstprojectServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		resp.setContentType("text/plain");
+		resp.setContentType("text/html");
 		resp.getWriter().println("Hello, Sapir Duke");
+		int num1, num2, num3;
+		num1= 4;
+		num2= 3;
+		num3= 7;
+		int result = ((num1+num2)*num3);
+		String resultStr = new String ("<h1>Result of ("+num1+"+"+num2+")*"+num3+"=" +result+"</h1>");
+		resp.getWriter().println(resultStr);
 	}
 }
