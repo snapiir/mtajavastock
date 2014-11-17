57ce4361cb2d59c65e7c297c1340f86f7bf1fce2
sapir <sapirduke@gmail.com>
2014-11-18 00:45:56 +0200

    Ex. 3

--- src/com/mta/javacourse/SapirfirstprojectServlet.java
+++ src/com/mta/javacourse/SapirfirstprojectServlet.java
@@ -14,5 +14,34 @@ public class SapirfirstprojectServlet extends HttpServlet {
 		int result = ((num1+num2)*num3);
 		String resultStr = new String ("<h1>Result of ("+num1+"+"+num2+")*"+num3+"=" +result+"</h1>");
 		resp.getWriter().println(resultStr);
+		
+		
+		int radius = 50;
+		double area = (Math.pow(radius,2)) * (Math.PI);
+		String circleArea = new String ("Area of circle with radius "+radius+" is "+area+" square-cm");
+		
+		int  hypotenuse = 50;
+		double angleB;
+		float opposite;
+		
+		angleB = 30 * (Math.PI/180);
+		opposite = (float)(hypotenuse * Math.sin(angleB));
+		String calcOpposite = new String ("Length of opposite where angle B is 30 degrees and hypotenuse lenght is "+hypotenuse+" cm is :"+opposite+" cm ");
+		
+		int base = 20;
+		int exp = 13;
+		double power = Math.pow(base,exp);
+		String calcPower = new String ("Power of "+base+" with exp of "+exp+" is "+power+" ");
+		
+		String line1 = new String (circleArea);
+		String line2 = new String (calcOpposite);
+		String line3 = new String (calcPower);
+		
+		String resultStr1 = line1 + "<br>" + line2 + "<br>" + line3;
+		resp.getWriter().println(resultStr1);
+		
+		
+		
+		
 	}
 }
